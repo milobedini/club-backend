f68f7cae27df8c46724f7d96665c4dc153a2a715
Milo Bedini <milobedini64@gmail.com>
2022-06-21 12:38:07 +0100

    club images

--- squad/models.py
+++ squad/models.py
@@ -1,16 +1,16 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
 
 # Create your models here.
 
-
+# NEED TO MAKE IMAGES OPTIONAL
 class Squad(models.Model):
     name = models.CharField(max_length=100)
-    # admin members
     sport = models.CharField(max_length=100)
     recurring = models.BooleanField(default=False)
     venue = models.CharField(max_length=100, default=None, blank=True)
     weekday = models.CharField(max_length=100, default=None, blank=True)
-    # club icon
+    image = CloudinaryField("image", default="")
 
     def __str__(self):
         return f"{self.name}"
