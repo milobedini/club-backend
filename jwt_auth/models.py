265e03c2bc948af510370be70841bda7f5bb0593
Milo Bedini <milobedini64@gmail.com>
2022-06-21 12:08:46 +0100

    user changes

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -7,10 +7,10 @@ from django.db import models
 class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=50)
-    squads = models.ManyToManyField("squad.Squad", related_name="members", default=None)
-    admin_squads = models.ManyToManyField("squad.Squad", related_name="admin_members", default=2)
+    squads = models.ManyToManyField("squad.Squad", related_name="members", default=None, blank=True)
+    admin_squads = models.ManyToManyField("squad.Squad", related_name="admin_members", blank=True)
     debt = models.SmallIntegerField(default=0)
-    attending = models.ManyToManyField("events.Event", related_name="participants", default=None)
+    attending = models.ManyToManyField("events.Event", related_name="participants", default=None, blank=True)
 
     def __str__(self):
         return f"{self.name}"
