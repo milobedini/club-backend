d59cbd6fcb109f903d904bc4afbc280a1d34c9bb
Milo Bedini <milobedini64@gmail.com>
2022-06-22 12:27:00 +0100

    fixed image being nullable

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,3 +1,4 @@
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,6 +12,7 @@ class User(AbstractUser):
     admin_squads = models.ManyToManyField("squad.Squad", related_name="admin_members", blank=True)
     debt = models.SmallIntegerField(default=0)
     attending = models.ManyToManyField("events.Event", related_name="participants", default=None, blank=True)
+    image = CloudinaryField("image", blank=True)
 
     def __str__(self):
         return f"{self.name}"
--- squad/models.py
+++ squad/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 # NEED TO MAKE IMAGES OPTIONAL
 class Squad(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     sport = models.CharField(max_length=100)
     recurring = models.BooleanField(default=False)
     venue = models.CharField(max_length=100, default=None, blank=True)
     weekday = models.CharField(max_length=100, default=None, blank=True)
-    image = CloudinaryField("image", default="")
+    image = CloudinaryField("image", blank=True)
 
     def __str__(self):
         return f"{self.name}"
