7a9b641822bad87dcde0f04f7e889e2bcff2d742
Milo Bedini <milobedini64@gmail.com>
2022-07-05 20:01:03 +0100

    change to six dp

--- events/models.py
+++ events/models.py
@@ -7,8 +7,8 @@ class Event(models.Model):
     club = models.ForeignKey("squad.Squad", related_name="events", on_delete=models.CASCADE)
     time = models.DateTimeField()
     location = models.CharField(max_length=100)
-    latitude = models.DecimalField(decimal_places=2, max_digits=6)
-    longitude = models.DecimalField(decimal_places=2, max_digits=6)
+    latitude = models.DecimalField(decimal_places=6, max_digits=12)
+    longitude = models.DecimalField(decimal_places=6, max_digits=12)
     total_cost = models.SmallIntegerField()
     total_players = models.SmallIntegerField()
     financier = models.ForeignKey(
